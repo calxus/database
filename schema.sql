c23d80913fe3f9080810a0604d6faed7e8746cea
Gordon Adam <GAdam@ybs.co.uk>
2020-01-17 13:40:18 +0000

    Adding menu schema to database

--- schema.sql
+++ schema.sql
@@ -10,7 +10,6 @@ CREATE TABLE `ashilda`.`login` (
   `expired` VARCHAR(45) NULL,
   PRIMARY KEY (`id`));
 
-
 CREATE TABLE `ashilda`.`post` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` INT NULL,
@@ -18,12 +17,32 @@ CREATE TABLE `ashilda`.`post` (
   `date` VARCHAR(128) NULL,
   PRIMARY KEY (`id`));
 
-
 CREATE TABLE `ashilda`.`user` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(128) NULL,
   `last_name` VARCHAR(128) NULL,
   `email` VARCHAR(128) NULL,
   `password` VARCHAR(256) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+
+CREATE TABLE `ashilda`.`course` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(128) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+
+CREATE TABLE `ashilda`.`dish` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `course_id` INT NULL,
+  `name` VARCHAR(128) NULL,
+  `vegetarian` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+
+CREATE TABLE `ashilda`.`order` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `dish_id` INT NULL,
+  `user_id` INT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC));
